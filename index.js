9d75b63ccee920988098166ae46b6d37472a3c01
Chase-A <58276610+Chase-A@users.noreply.github.com>
2022-01-26 11:39:59 -0500

    circles locked

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/innerWidth
+const vw = window.innerWidth
+const redCircle = document.getElementById("red-circle")
+const greenCircle = document.getElementById("green-circle")
+alignShapes()
+
+function alignShapes(){
+    const centerLine = vw/2
+
+    redCircle.setAttribute("cx", centerLine+205)
+    greenCircle.setAttribute("cx", centerLine-205)
+}
+
+// circle.setAttribute("cx", "150")
+
+
+// keeps shapes locked
+window.onresize = function(){ location.reload(); }
